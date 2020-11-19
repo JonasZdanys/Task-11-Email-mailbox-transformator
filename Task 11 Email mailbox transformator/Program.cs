ed58b6d608c0e6a0ab8d0aee6d9dd5c114a4d76d
Jonas Zdanys <jonas.zdanys@teliacompany.com>
2020-11-19 14:23:07 +0200

    working on as list

---
+++ Task 11 Email mailbox transformator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.Json;
+
+
+namespace Task_11_Email_mailbox_transformator
+{
+    class Program
+    {
+        static string ReadString(string prompt)
+        {
+            string result;
+            do
+            {
+                Console.Write(prompt);
+                result = Console.ReadLine();
+            } 
+            while (result == "");
+            return result;
+        }
+        static void Main()
+        {
+            string content = File.ReadAllText(@"C:\Temp\Misc\InfoForComms.txt"); //(ReadString("Please provide the path and file name: "));
+            List<string> rows = new List<string>(content.Split('\n'));
+            var data = new Dictionary<string, string>(); 
+            Console.WriteLine($"Result list: {JsonSerializer.Serialize(rows)}");
+
+            foreach (var row in rows)
+            {
+                foreach (var col in row.Trim().Split('\t'))
+                {
+                    Console.WriteLine(col);
+                }
+            }
+        }
+    }
+}
