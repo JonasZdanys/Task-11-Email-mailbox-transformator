2b98ebb7684eb685b22901e801c10f6a466a2513
Jonas Zdanys <jonas.zdanys@teliacompany.com>
2020-11-20 15:04:18 +0200

    does not work

--- Task 11 Email mailbox transformator/Program.cs
+++ Task 11 Email mailbox transformator/Program.cs
@@ -25,8 +25,35 @@ namespace Task_11_Email_mailbox_transformator
             List<string> rows = new List<string>(content.Split('\n'));
             var data = new Dictionary<string, string>();
             //Console.WriteLine($"Result list: {JsonSerializer.Serialize(rows)}");
-            foreach (var row in rows)
+            for (int i = 0; i < rows.Capacity; i++)
             {
+                var col = rows[i].Trim().Split('\t');
+                if (col[1] != null)
+                {
+                    if (col[1].Contains(","))
+                    {
+                        foreach (var email in col[1].Split(','))
+                        {
+                            //Console.WriteLine(email);
+                            try
+                            {
+                                data.Add(email, col[0]);
+                            }
+                            catch (ArgumentException)
+                            {
+                                Console.WriteLine("An element with Key = \"email\" already exists.");
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    col[1] = "Mailbox col[0] does not have an owner";
+                    data.Add(col[1], col[0]);
+                }
+            }
+            Console.WriteLine($"Result list: {JsonSerializer.Serialize(data)}");
+            /*{
                 foreach (var col in row.Trim().Split('\t'))
                 {
                     //Console.WriteLine(col);
@@ -36,7 +63,7 @@ namespace Task_11_Email_mailbox_transformator
                         x++;
                     }
                 }
-            }
+            }*/
         }
     }
 }
